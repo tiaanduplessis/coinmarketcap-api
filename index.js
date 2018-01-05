f9fd8232206b528a66d370928433fb8699afbd3a
Tiaan du Plessis <tiaanduplessis@hotmail.com>
2018-01-05 11:31:31 +0200

    Add config option

--- index.js
+++ index.js
@@ -6,47 +6,54 @@ const qs = require('qs')
 const BASE_URL = 'https://api.coinmarketcap.com/'
 
 class CoinMarketCap {
-  constructor ({ version = 'v1' } = {}) {
-    this.headers = {
-      Accept: 'application/json',
-      'Accept-Charset': 'utf-8'
-    }
+  constructor ({ version = 'v1', config = {} } = {}) {
+    this.config = Object.assign(
+      {},
+      {
+        headers: {
+          Accept: 'application/json',
+          'Accept-Charset': 'utf-8'
+        },
+        method: 'GET'
+      },
+      config
+    )
+
     this.url = `${BASE_URL}/${version}`
   }
 
   /**
-     * Get ticker information
-     *
-     * @param {Object=} options Options for the request:
-     * @param {Int=} options.limit  Only returns the top limit results
-     * @param {String=} options.convert  Return price, 24h volume, and market cap in terms of another currency
-     * @param {String=} options.currency  Return only specific currency
-     *
-     * @example
-     * const client = new CoinMarketCap()
-     * client.getTicker({limit: 3}).then(console.log).catch(console.error)
-     * client.getTicker({limit: 1, currency: 'bitcoin'}).then(console.log).catch(console.error)
-     * client.getTicker({convert: 'EUR'}).then(console.log).catch(console.error)
-     */
+   * Get ticker information
+   *
+   * @param {Object=} options Options for the request:
+   * @param {Int=} options.limit  Only returns the top limit results
+   * @param {String=} options.convert  Return price, 24h volume, and market cap in terms of another currency
+   * @param {String=} options.currency  Return only specific currency
+   *
+   * @example
+   * const client = new CoinMarketCap()
+   * client.getTicker({limit: 3}).then(console.log).catch(console.error)
+   * client.getTicker({limit: 1, currency: 'bitcoin'}).then(console.log).catch(console.error)
+   * client.getTicker({convert: 'EUR'}).then(console.log).catch(console.error)
+   */
   getTicker ({ limit, convert, currency }) {
     return createRequest({
       url: `${this.url}/ticker${currency ? `/${currency}`.toLowerCase() : ''}`,
-      headers: this.headers,
       query: { convert, limit }
     })
   }
 
   /**
-     * Get global information
-     *
-     * @param {Object|String=} options  Options for the request
-     * @param {String=} options.convert  Return price, 24h volume, and market cap in terms of another currency
-     *
-     * @example
-     * const client = new CoinMarketCap()
-     * client.getGlobal('GBP').then(console.log).catch(console.error)
-     * client.getGlobal({convert: 'GBP'}).then(console.log).catch(console.error)
-     */
+   * Get global information
+   *
+   * @param {Object|String=} options  Options for the request
+   * @param {String=} options.convert  Return price, 24h volume, and market cap in terms of another currency
+   *
+   * @example
+   * const client = new CoinMarketCap()
+   * client.getGlobal('GBP').then(console.log).catch(console.error)
+   * client.getGlobal({convert: 'GBP'}).then(console.log).catch(console.error)
+   */
   getGlobal (convert) {
     if (typeof convert === 'string') {
       convert = { convert: convert.toUpperCase() }
@@ -54,21 +61,16 @@ class CoinMarketCap {
 
     return createRequest({
       url: `${this.url}/global`,
-      headers: this.headers,
       query: convert
     })
   }
 }
 
-const createRequest = ({ url, headers, query }) => {
-  const opts = {
-    headers,
-    method: 'GET'
-  }
-
-  return fetch(`${url}${query ? `?${qs.stringify(query)}` : ''}`).then(res =>
-    res.json()
-  )
+const createRequest = ({ url, query }) => {
+  return fetch(
+    `${url}${query ? `?${qs.stringify(query)}` : ''}`,
+    this.config
+  ).then(res => res.json())
 }
 
 module.exports = CoinMarketCap
