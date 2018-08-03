cfcf36266c9241273723c0094d7c0ac179706302
Tiaan du Plessis <tiaanduplessis@hotmail.com>
2018-08-03 06:15:14 +0200

    Start updating tests

--- test.js
+++ test.js
@@ -2,131 +2,131 @@ require('jest-extended')
 require('jest-chain')
 const CoinMarketCap = require('./')
 
+const API_KEY = process.env.COINMARKETCAP_API_KEY
+
 test('should be defined', () => {
   expect(CoinMarketCap).toBeDefined()
 })
 
 test('should return new CoinMarketCap client', () => {
-  const client = new CoinMarketCap()
-  expect(client.getTicker).toBeDefined()
+  const client = new CoinMarketCap(API_KEY)
+  expect(client.getTickers).toBeDefined()
   expect(client.getGlobal).toBeDefined()
-  expect(client.getListings).toBeDefined()
+  expect(client.getQuotes).toBeDefined()
 })
 
-test('getTicker should have correct response structure and type', async () => {
-  const client = new CoinMarketCap()
-  const ticker = await client.getTicker()
-
-  expect(ticker).toContainAllKeys(['data', 'metadata'])
-  expect(ticker).toHaveProperty('metadata.timestamp')
-  expect(ticker).toHaveProperty('metadata.num_cryptocurrencies')
-  expect(ticker).toHaveProperty('metadata.error')
-  expect(ticker.data).toBeObject().not.toBeArray()
-  expect(ticker.metadata.timestamp).toBeNumber()
-  expect(ticker.metadata['num_cryptocurrencies']).toBeNumber()
+test('getTickers should have correct response structure and type', async () => {
+  const client = new CoinMarketCap(API_KEY)
+  const ticker = await client.getTickers()
+  expect(ticker).toContainAllKeys(['data', 'status'])
+  expect(ticker).toHaveProperty('status.timestamp')
+  expect(ticker).toHaveProperty('status.credit_count')
+  expect(ticker).toHaveProperty('status.error_code')
+  expect(ticker.data).toBeArray()
+  expect(ticker.status.timestamp).toBeString()
 })
 
-test('should get latest ticker', async () => {
-  const client = new CoinMarketCap()
-  const ticker1 = await client.getTicker()
-  const ticker2 = await client.getTicker({limit: 10})
+test('should get latest tickers', async () => {
+  const client = new CoinMarketCap(API_KEY)
+  const ticker1 = await client.getTickers()
+  const ticker2 = await client.getTickers({limit: 10})
 
   expect(typeof ticker1).toBe('object')
   expect(typeof ticker2).toBe('object')
   expect(Object.keys(ticker2.data).length).toBe(10)
 })
 
-test('should get Bitcoin ticker from currency symbol', async () => {
-  const client = new CoinMarketCap()
-  const ticker = await client.getTicker({currency: 'BTC'})
+// test('should get Bitcoin ticker from currency symbol', async () => {
+//   const client = new CoinMarketCap(API_KEY)
+//   const ticker = await client.getTickers({currency: 'BTC'})
 
-  expect(typeof ticker).toBe('object')
-  expect(ticker.data.id).toBe(1)
-  expect(ticker.data.name).toMatch('Bitcoin')
-  expect(ticker.data.symbol).toMatch('BTC')
-})
+//   expect(typeof ticker).toBe('object')
+//   expect(ticker.data.id).toBe(1)
+//   expect(ticker.data.name).toMatch('Bitcoin')
+//   expect(ticker.data.symbol).toMatch('BTC')
+// })
 
-test('should get Bitcoin ticker from its listings ID', async () => {
-  const client = new CoinMarketCap()
-  const ticker = await client.getTicker({ id: 1 })
+// test('should get Bitcoin ticker from its listings ID', async () => {
+//   const client = new CoinMarketCap(API_KEY)
+//   const ticker = await client.getTicker({ id: 1 })
 
-  expect(typeof ticker).toBe('object')
-  expect(ticker.data.id).toBe(1)
-  expect(ticker.data.name).toMatch('Bitcoin')
-  expect(ticker.data.symbol).toMatch('BTC')
-})
+//   expect(typeof ticker).toBe('object')
+//   expect(ticker.data.id).toBe(1)
+//   expect(ticker.data.name).toMatch('Bitcoin')
+//   expect(ticker.data.symbol).toMatch('BTC')
+// })
 
-test('should get tickers sorted by ID', async () => {
-  const client = new CoinMarketCap()
-  const ticker = await client.getTicker({sort: 'id'})
+// test('should get tickers sorted by ID', async () => {
+//   const client = new CoinMarketCap(API_KEY)
+//   const ticker = await client.getTicker({sort: 'id'})
 
-  let data = Object.values(ticker.data)
-  for (let i = 1; i < data.length; i++) {
-    expect(data[i].id).toBeGreaterThan(data[i - 1].id)
-  }
-})
+//   let data = Object.values(ticker.data)
+//   for (let i = 1; i < data.length; i++) {
+//     expect(data[i].id).toBeGreaterThan(data[i - 1].id)
+//   }
+// })
 
-test('should return main data field as an array', async () => {
-  const client = new CoinMarketCap()
-  const ticker = await client.getTicker({structure: 'array'})
+// test('should return main data field as an array', async () => {
+//   const client = new CoinMarketCap(API_KEY)
+//   const ticker = await client.getTicker({structure: 'array'})
 
-  expect(Array.isArray(ticker.data)).toBeTruthy()
-})
+//   expect(Array.isArray(ticker.data)).toBeTruthy()
+// })
 
-test('should result in errors due to wrong parameter usage', async () => {
-  const client = new CoinMarketCap()
+// test('should result in errors due to wrong parameter usage', async () => {
+//   const client = new CoinMarketCap(API_KEY)
 
-  try {
-    await client.getTicker({currency: 'BTC', id: 1})
-  } catch (e) {
-    expect(e.toString()).toMatch('Error: Currency and ID cannot be passed in at the same time.')
-  }
+//   try {
+//     await client.getTicker({currency: 'BTC', id: 1})
+//   } catch (e) {
+//     expect(e.toString()).toMatch('Error: Currency and ID cannot be passed in at the same time.')
+//   }
 
-  try {
-    await client.getTicker({start: 1, currency: 'ETH'})
-  } catch (e) {
-    expect(e.toString()).toMatch('Error: Start, limit, and sort options can only be used when currency or ID is not given.')
-  }
-})
+//   try {
+//     await client.getTicker({start: 1, currency: 'ETH'})
+//   } catch (e) {
+//     expect(e.toString()).toMatch('Error: Start, limit, and sort options can only be used when currency or ID is not given.')
+//   }
+// })
 
-test('should get latest global', async () => {
-  const client = new CoinMarketCap()
-  const global = await client.getGlobal()
+// test('should get latest global', async () => {
+//   const client = new CoinMarketCap()
+//   const global = await client.getGlobal()
 
-  expect(typeof global).toBe('object')
-})
+//   expect(typeof global).toBe('object')
+// })
 
-test('should get latest listings', async () => {
-  const client = new CoinMarketCap()
-  const listings = await client.getListings()
+// test('should get latest listings', async () => {
+//   const client = new CoinMarketCap(API_KEY)
+//   const listings = await client.getListings()
 
-  expect(typeof listings).toBe('object')
-  expect(Array.isArray(listings.data)).toBe(true)
-})
+//   expect(typeof listings).toBe('object')
+//   expect(Array.isArray(listings.data)).toBe(true)
+// })
 
-test('should result in error if limit = 0 and start specified', async () => {
-  const client = new CoinMarketCap()
+// test('should result in error if limit = 0 and start specified', async () => {
+//   const client = new CoinMarketCap(API_KEY)
 
-  try {
-    await client.getTicker({start: 1, limit: 0})
-  } catch (e) {
-    expect(e.toString()).toMatch('Error: Start and limit = 0 cannot be passed in at the same time.')
-  }
-})
+//   try {
+//     await client.getTicker({start: 1, limit: 0})
+//   } catch (e) {
+//     expect(e.toString()).toMatch('Error: Start and limit = 0 cannot be passed in at the same time.')
+//   }
+// })
 
-test('should return data field as an array if specified structure as an array', async () => {
-  const client = new CoinMarketCap()
-  const ticker = await client.getTicker({structure: 'array'})
+// test('should return data field as an array if specified structure as an array', async () => {
+//   const client = new CoinMarketCap(API_KEY)
+//   const ticker = await client.getTicker({structure: 'array'})
 
-  expect(ticker.data).toBeArray()
-})
+//   expect(ticker.data).toBeArray()
+// })
 
-test('should return all tickers', async () => {
-  const client = new CoinMarketCap()
-  const totalActiveCryptocurrencies = await client.getGlobal().then(res => res.data['active_cryptocurrencies'])
-  const allTickers1 = await client.getTicker({limit: '0'}).then(res => Object.keys(res.data).length)
-  const allTickers2 = await client.getTicker({limit: '0', structure: 'array'}).then(res => res.data.length)
+// test('should return all tickers', async () => {
+//   const client = new CoinMarketCap(API_KEY)
+//   const totalActiveCryptocurrencies = await client.getGlobal().then(res => res.data['active_cryptocurrencies'])
+//   const allTickers1 = await client.getTicker({limit: '0'}).then(res => Object.keys(res.data).length)
+//   const allTickers2 = await client.getTicker({limit: '0', structure: 'array'}).then(res => res.data.length)
 
-  expect(allTickers1).toBe(totalActiveCryptocurrencies)
-  expect(allTickers2).toBe(totalActiveCryptocurrencies)
-})
+//   expect(allTickers1).toBe(totalActiveCryptocurrencies)
+//   expect(allTickers2).toBe(totalActiveCryptocurrencies)
+// })
